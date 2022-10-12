0f6b713d1de5c3d90a9467225211122f37354240
Denys Yevchenko <31778102+JohnFrisby@users.noreply.github.com>
2022-10-12 22:30:35 +0300

    withRouter

--- src/components/pages/people-page.js
+++ src/components/pages/people-page.js
@@ -1,24 +1,14 @@
-import React, { Component } from "react";
-import Row from '../row';
-import { PersonDetails, PersonList } from '../sw-components';
+import React from "react";
+import { PersonList } from '../sw-components';
+import { withRouter } from "react-router-dom";
 
-export default class PeoplePage extends Component {
-
-    state = {
-        selectedItem: null
-    };
-
-    onItemSelected = (selectedItem) => {
-        this.setState({ selectedItem });
-    };
-
-    render() {
-        const { selectedItem } = this.state;
+const PeoplePage = ({ history }) => {
 
         return (
-            <Row
-                left={<PersonList onItemSelected={this.onItemSelected} />}
-                right={<PersonDetails itemId={selectedItem} />} />
+            <PersonList 
+            onItemSelected={(itemId) => {
+                history.push(`/people/${itemId}`)
+            }} />
         );
-    }
-}
+    };
+export default withRouter(PeoplePage);
